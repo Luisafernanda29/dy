41cc22f9a8b58b97b00be76409e63d10654a1e8c
Luisa Fernanda Cortezano Movilla <luisacortezano135@gmail.com>
2023-11-30 05:51:40 -0500

    creado el proyecto inicial

---
+++ Cesar/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'home#dashboard'
+  resources :pedidos
+  resources :clientes
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
